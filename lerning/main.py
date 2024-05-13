14954c541d9d31a0664d84b5416c3c7058905e62
Rodion Kuznetsov <keksonatorver2@gmail.com>
2024-05-13 15:11:26 +0300

    добавлен гит игнор + токен бота записан в конфиг

--- lerning/main.py
+++ lerning/main.py
@@ -1,8 +1,5 @@
-qvt = int(input())
+date_time = input()
+dollar = input()
+uani = input()
 
-for i in range(1, qvt+1):
-    comment = input()
-    if comment.isspace() or comment == '':
-        print(f"{i}: COMMENT SHOULD BE DELETED")
-    else:
-        print(f"{i}: {comment}")
+print(f"На {date_time}: 1$ = {dollar}₽, 1¥ = {uani}₽")
